bfd4715acb43e4599668f79f4702fd93cb0d2668
nordffa <nordffa@gmail.com>
2025-09-09 13:33:15 -0300

    Adicionado novas funções

--- models.py
+++ models.py
@@ -61,3 +61,50 @@ def desativar_produto(cod_produto):
             conn.commit()
             cur.close()
             conn.close()
+
+
+def inserir_cliente(nome_completo, cpf, ativo="TRUE"):
+    """
+    Funcao que insere um cliente no banco de dados
+    """
+    conn = get_connection()
+    if conn:
+        cur = conn.cursor()
+        cur.execute(
+            "INSERT INTO cliente (nome_completo, cpf, ativo) VALUES (%s, %s, %s)",
+            (nome_completo, cpf, ativo)
+        )
+        conn.commit()
+        cur.close()
+        conn.close()
+
+
+def listar_clientes():
+    """
+    Funcao que lista todos os clientes cadastrados
+    """
+    conn = get_connection()
+    produtos = []
+    if conn:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM cliente WHERE ativo = TRUE ORDER BY cod_cliente")
+        produtos = cur.fetchall()
+        cur.close()
+        conn.close()
+    return produtos
+
+
+def desativar_cliente(cod_cliente):
+    """
+    Funcao que muda o status do cliente para "FALSE" e remove da visualizacao de todos os clientes
+    """
+    conn = get_connection()
+    if conn:
+            cur = conn.cursor()
+            cur.execute(
+                "UPDATE cliente SET ativo = FALSE WHERE cod_cliente = %s",
+                (cod_cliente,)
+            )
+            conn.commit()
+            cur.close()
+            conn.close()
