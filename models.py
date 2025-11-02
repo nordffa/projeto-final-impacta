976fed275baed8bbe3dcd77b62b06639d238c1f8
nordffa <nordffa@gmail.com>
2025-11-02 19:58:33 -0300

    Adicionado as funcoes relacionadas ao cadastro de fornecedores

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from db import get_connection
 
 
+# PRODUTOS
 def inserir_produto(descricao, valor_unitario, qtde_estoque, fornecedor, ativo="TRUE"):
     """
     Funcao que insere produtos no banco de dados
@@ -63,6 +64,7 @@ def desativar_produto(cod_produto):
         conn.close()
 
 
+# CLIENTES
 def inserir_cliente(nome_completo, cpf, ativo="TRUE"):
     """
     Funcao que insere um cliente no banco de dados
@@ -84,14 +86,14 @@ def listar_clientes():
     Funcao que lista todos os clientes cadastrados
     """
     conn = get_connection()
-    produtos = []
+    clientes = []
     if conn:
         cur = conn.cursor()
         cur.execute("SELECT * FROM cliente WHERE ativo = TRUE ORDER BY cod_cliente")
-        produtos = cur.fetchall()
+        clientes = cur.fetchall()
         cur.close()
         conn.close()
-    return produtos
+    return clientes
 
 
 def desativar_cliente(cod_cliente):
@@ -103,6 +105,56 @@ def desativar_cliente(cod_cliente):
         cur = conn.cursor()
         cur.execute(
             "UPDATE cliente SET ativo = FALSE WHERE cod_cliente = %s", (cod_cliente,)
+        )
+        conn.commit()
+        cur.close()
+        conn.close()
+
+
+# FORNECEDORES
+def inserir_fornecedor(nome_fantasia, razao_social, cnpj, ativo):
+    """
+    Funcao que insere fornecedores no banco de dados
+    """
+    conn = get_connection()
+    if conn:
+        cur = conn.cursor()
+        cur.execute(
+            "INSERT INTO fornecedores (nome_fantasia, razao_social, cnpj, ativo) VALUES (%s, %s, %s, %s)",
+            (nome_fantasia, razao_social, cnpj, ativo),
+        )
+        conn.commit()
+        cur.close()
+        conn.close()
+
+
+def listar_fornecedores():
+    """
+    Funcao que lista todos os fornecedores cadastrados
+    """
+    conn = get_connection()
+    fornecedores = []
+    if conn:
+        cur = conn.cursor()
+        cur.execute(
+            "SELECT * FROM fornecedores WHERE ativo = TRUE ORDER BY cod_fornecedor"
+        )
+        fornecedores = cur.fetchall()
+        cur.close()
+        conn.close()
+    return fornecedores
+
+
+def desativar_fornecedor(cod_fornecedor):
+    """
+    Funcao que muda o status do fornecedor para "FALSE" e remove da visualizacao de todos os clientes
+    """
+    conn = get_connection()
+    if conn:
+        cur = conn.cursor()
+        cur.execute(
+            "UPDATE fornecedores SET ativo = FALSE WHERE cod_fornecedor = %s",
+            (cod_fornecedor,),
         )
         conn.commit()
         cur.close()
